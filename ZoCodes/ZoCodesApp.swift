5d916de23addb094cc77bc9062d6581e7b56442f
Zoya Afreen <zoyafreen04@gmail.com>
2023-06-14 18:47:28 +0530

    Initial Commit

---
+++ ZoCodes/ZoCodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoCodesApp.swift
+//  ZoCodes
+//
+//  Created by Zoya Afreen on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ZoCodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
